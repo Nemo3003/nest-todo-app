862ebe5ad270aa148a2fad16e631d658c81bd976
nemo3003 <ulimarian2000@gmail.com>
2023-08-09 15:09:12 -0300

    [UPD] Create and get tasks completed, U and D still missing

---
+++ nestfirstapi/src/tasks/tasks.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDTO } from './dto/task.dto';
+
+@Controller('tasks')
+export class TasksController {
+
+    constructor(private taskService : TasksService) {
+        
+    }
+
+    @Get()
+    getAllTasks(){
+        return this.taskService.getAllTasks();
+    }
+
+    @Post()
+    createTask(@Body() newTask: CreateTaskDTO) { 
+        return this.taskService.createTask(newTask.title, newTask.description);
+    }
+}
